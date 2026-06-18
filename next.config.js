60b54f046cec7fd40fe612755f0215ad76b953fe
назрОС <102623319+nazrOS-softhard@users.noreply.github.com>
2026-06-18 03:40:10 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // Разрешаем загрузку скриптов с внешних доменов для MindAR
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'credentialless',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
